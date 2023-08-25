577308e76ba755b1bffef29a9771ac13260349fc
valentin-morette <morette.valentin@gmail.com>
2023-08-25 20:06:19 +0200

    start

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,120 @@
-import Home from "@pages/Home";
-
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <h1>Pizzeria "le charbon"</h1>
+      <div className="base">
+        <h2>Menu</h2>
+        <div className="primary">
+          <h3>Pizzas</h3>
+          <div className="primaryPizza">
+            <div className="secondaryPizza">
+              <div>
+                <h4>4 fromages</h4>
+                <p>Emmental, Gruyère, Chèvre, Roquefort</p>
+              </div>
+              <p>12€</p>
+            </div>
+            <div className="secondaryPizza">
+              <div>
+                <h4>4 fromages</h4>
+                <p>Emmental, Gruyère, Chèvre, Roquefort</p>
+              </div>
+              <p>12€</p>
+            </div>
+            <div className="secondaryPizza">
+              <div>
+                <h4>4 fromages</h4>
+                <p>Emmental, Gruyère, Chèvre, Roquefort</p>
+              </div>
+              <p>12€</p>
+            </div>
+            <div className="secondaryPizza">
+              <div>
+                <h4>4 fromages</h4>
+                <p>Emmental, Gruyère, Chèvre, Roquefort</p>
+              </div>
+              <p>12€</p>
+            </div>
+            <div className="secondaryPizza">
+              <div>
+                <h4>4 fromages</h4>
+                <p>Emmental, Gruyère, Chèvre, Roquefort</p>
+              </div>
+              <p>12€</p>
+            </div>
+            <div className="secondaryPizza">
+              <div>
+                <h4>4 fromages</h4>
+                <p>Emmental, Gruyère, Chèvre, Roquefort</p>
+              </div>
+              <p>12€</p>
+            </div>
+            <div className="secondaryPizza">
+              <div>
+                <h4>4 fromages</h4>
+                <p>Emmental, Gruyère, Chèvre, Roquefort</p>
+              </div>
+              <p>12€</p>
+            </div>
+            <div className="secondaryPizza">
+              <div>
+                <h4>4 fromages</h4>
+                <p>Emmental, Gruyère</p>
+              </div>
+              <p>12€</p>
+            </div>
+            <div className="secondaryPizza">
+              <div>
+                <h4>4 fromages</h4>
+                <p>Emmental, Gruyère</p>
+              </div>
+              <p>12€</p>
+            </div>
+          </div>
+        </div>
+        <div className="secondary">
+          <div className="secondaryBloc">
+            <h3>Boissons</h3>
+            <div className="blocSecond">
+              <div className="secondaryPizza">
+                <div>
+                  <h4>Coca-Cola</h4>
+                  <p>33cl</p>
+                </div>
+                <p>2€</p>
+              </div>
+              <div className="secondaryPizza">
+                <div>
+                  <h4>Fanta</h4>
+                  <p>33cl</p>
+                </div>
+                <p>2€</p>
+              </div>
+            </div>
+          </div>
+          <div className="secondaryBloc">
+            <h3>Salades</h3>
+            <div className="blocSecond">
+              <div className="secondaryPizza">
+                <div>
+                  <h4>Composé</h4>
+                  <p>Salade, Tomate, Oeuf, Jambon, Emmental</p>
+                </div>
+                <p>2€</p>
+              </div>
+              <div className="secondaryPizza">
+                <div>
+                  <h4>Composé</h4>
+                  <p>Salade, Tomate, Oeuf, Jambon, Emmental</p>
+                </div>
+                <p>2€</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
